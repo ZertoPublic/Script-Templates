e7af29ae08ecb68b60454c33107ddbca646de58b
Justin Paul <justin@jpaul.me>
2018-04-19 16:22:25 -0400

    Initial Commit
    Created .ps1 code snippet for use in other powershell rest scripts to ignore self signed ssl certs

---
+++ TIP-PoSH-Ignore-Self-Signed-SSL.ps1
@@ -0,0 +1,36 @@
+# this snippet can be used above Invoke-WebRequest  commands in order to bypass the error you will get if you have a 
+# self signed ssl certificate on your vcenter or ZVM server.
+# Simply paste this code at the start of your powershell script and it will solve self signed SSL issues when using REST APIs.
+
+# ignore self signed SSL
+if (-not ([System.Management.Automation.PSTypeName]'ServerCertificateValidationCallback').Type)
+{
+$certCallback = @"
+    using System;
+    using System.Net;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
+    public class ServerCertificateValidationCallback
+    {
+        public static void Ignore()
+        {
+            if(ServicePointManager.ServerCertificateValidationCallback ==null)
+            {
+                ServicePointManager.ServerCertificateValidationCallback += 
+                    delegate
+                    (
+                        Object obj, 
+                        X509Certificate certificate, 
+                        X509Chain chain, 
+                        SslPolicyErrors errors
+                    )
+                    {
+                        return true;
+                    };
+            }
+        }
+    }
+"@
+    Add-Type $certCallback
+ }
+[ServerCertificateValidationCallback]::Ignore()
