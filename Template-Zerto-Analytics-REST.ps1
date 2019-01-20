a09778aa5d173d31311861210f7c6fc5117d31ec
Justin Paul <justin@jpaul.me>
2019-01-19 23:13:29 -0500

    Create Template-Zerto-Analytics-REST.ps1
    Zerto Analytics Template Script that lists sites and some status information.

---
+++ Template-Zerto-Analytics-REST.ps1
@@ -0,0 +1,44 @@
+#################################################################
+# Template-Zerto-Analytics-REST.ps1
+# 
+# By Justin Paul, Zerto Technical Alliances Architect
+# Contact info: jp@zerto.com
+# Repo: https://www.github.com/Zerto-ta-public/Script-Templates
+#
+#################################################################
+
+################ Variables for your script ######################
+
+$strZaUrl = "analytics.api.zerto.com"
+$strZAUser = "me@mydomain.com"
+$strZAPwd = "myPassword"
+
+## Perform authentication so that Zerto APIs can run. Return a session identifier that needs tobe inserted in the header for subsequent requests.
+function getZertoAnalyticsAuthToken ($userName, $password){
+    $baseURL = "https://" + $strZaUrl
+    $xZertoSessionURL = $baseURL +"/v2/auth/token"
+    $params = @{
+        "username"="justin.paul@zerto.com"
+        "password"="Zertodata1!"
+    }
+    $contentType = "application/json"
+    $xZertoSessionResponse = Invoke-RESTMethod -Uri $xZertoSessionURL -Method POST -Body ($params|ConvertTo-Json) -ContentType $contentType
+    return $xZertoSessionResponse.token
+}
+
+#Extract x-zerto-session from the response, and add it to the actual API:
+$xZertoSession = getZertoAnalyticsAuthToken $strZVMUser $strZVMPwd
+$zertoSessionHeader = @{"accept"="application/json"
+    'Authorization' = "Bearer $($xZertoSession)"}
+
+################ Your Script starts here #######################
+#Invoke the Zerto API:
+$peerListApiUrl = "https://" + $strZaUrl + "/v2/monitoring/sites"
+$peerListJSON = Invoke-RestMethod -Uri $peerListApiUrl -H $zertoSessionHeader
+
+#Iterate with JSON:
+foreach ($peer in $peerListJSON){
+    Write-Host $peer.zvmIp  $peer.name $peer.type $peer.version $peer.connectionStatus
+}
+
+##End of script
